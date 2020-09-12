fa983d6ecd2d03183505da81e9a626f0018c0f93
ahmad <ahmad13932013@gmail.com>
2020-09-12 18:27:58 +0300

    saf

--- src/main/kotlin/org/example/App.kt
+++ src/main/kotlin/org/example/App.kt
@@ -7,16 +7,28 @@ import io.jooby.annotations.Path
 import io.jooby.runApp
 
 @Path("/")
-object Hello{
+class Hello{
     @GET
     fun hello(): String{
         return "Hello World"
     }
 }
-
+@Path("/hi")
+class Hi{
+    @GET
+    fun hi(): String{
+        return "Hi World"
+    }
+}
 class App : Kooby({
     install(OpenAPIModule())
-    mvc(Hello)
+    mvc(Hi())
+    routes {
+        before {
+            // do something that is specific to Hello controller
+        }
+        mvc(Hello())
+    }
 })
 fun main(args: Array<String>){
     runApp(args,App::class)
